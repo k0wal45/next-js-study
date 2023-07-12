addfb0c76edc77b7f0b1c776a8d94b055a7e5b42
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2023-07-12 11:54:30 +0200

    Add files via upload

---
+++ src/app/(shop)/product-2/page.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+function page() {
+  return (
+    <div className="w-full grid place-items-center gap-4">
+        <h4 className="text-bold text-4xl">Product 2</h4>
+        <ul className='flex gap-4 text-2xl'>
+            <li>
+                <Link href='/product-1'>Product 1</Link>
+            </li>
+            <li>
+                <Link href='/product-2'>Product 2</Link>
+            </li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default page
